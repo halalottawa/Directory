11165d44bf8a62b992cd2704abe0726a1978e283
Halal Ottawa <55626964+halalottawa@users.noreply.github.com>
2026-06-10 17:19:31 -0400

    Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,6 +2,12 @@ const config = {
   appId: 'com.app.halaottawaapk',
   appName: 'Halal Ottawa',
   webDir: 'dist',
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ['google.com'],
+    },
+  },
 };
 
 export default config;
